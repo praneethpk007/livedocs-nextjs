9975dc469e3d389006f36954310f32ffb18cc94f
praneethpk007 <210020014@iitdh.ac.in>
2024-08-31 01:30:48 +0530

    logo and editor included

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+type HeaderProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+const Header = ({ children }: HeaderProps) => {
+  return (
+    <div className="header">
+      <Link href={`/`}>
+        <Image
+          src="/assets/icons/logo.svg"
+          alt="LiveDocs"
+          width={120}
+          height={32}
+          className="hidden md:block"
+        />
+        <Image
+          src="/assets/icons/logo-icon.svg"
+          alt="LiveDocs"
+          width={32}
+          height={32}
+          className="mr-2 md:hidden"
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default Header;
