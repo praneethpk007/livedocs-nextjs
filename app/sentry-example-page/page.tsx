ce10e566d930676e87e32ccf59ebd62116a4b81a
praneethpk007 <210020014@iitdh.ac.in>
2024-09-01 03:21:22 +0530

    sentry changes

--- app/sentry-example-page/page.tsx
+++ app/sentry-example-page/page.tsx
@@ -49,15 +49,18 @@ export default function Page() {
             margin: "18px",
           }}
           onClick={async () => {
-            await Sentry.startSpan({
-              name: 'Example Frontend Span',
-              op: 'test'
-            }, async () => {
-              const res = await fetch("/api/sentry-example-api");
-              if (!res.ok) {
-                throw new Error("Sentry Example Frontend Error");
+            await Sentry.startSpan(
+              {
+                name: "Example Frontend Span",
+                op: "test",
+              },
+              async () => {
+                const res = await fetch("/api/sentry-example-api");
+                if (!res.ok) {
+                  throw new Error("Sentry Example Frontend Error");
+                }
               }
-            });
+            );
           }}
         >
           Throw error!
@@ -65,7 +68,10 @@ export default function Page() {
 
         <p>
           Next, look for the error on the{" "}
-          <a href="https://jsm-x9.sentry.io/issues/?project=4507159179034624">Issues Page</a>.
+          <a href="https://praneeth-karthikeya.sentry.io/issues/?project=4507159179034624">
+            Issues Page
+          </a>
+          .
         </p>
         <p style={{ marginTop: "24px" }}>
           For more information, see{" "}
