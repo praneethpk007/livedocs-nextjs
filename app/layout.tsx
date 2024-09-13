e0415624094e6d9225e53c968b9cbbdc2f8e6a88
praneethpk007 <210020014@iitdh.ac.in>
2024-09-13 20:49:14 +0530

    minor change to clerk layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,37 @@
-import { Inter as FontSans } from "next/font/google"
+import { Inter as FontSans } from "next/font/google";
 
-import { cn } from "@/lib/utils"
-import './globals.css'
-import { Metadata } from "next"
-import { ClerkProvider } from "@clerk/nextjs"
-import { dark } from "@clerk/themes"
-import Provider from "./Provider"
+import { cn } from "@/lib/utils";
+import "./globals.css";
+import { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import Provider from "./Provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const metadata: Metadata = {
-  title: 'LiveDocs',
-  description: 'Your go-to collaborative editor',
-}
+  title: "LiveDocs",
+  description: "Your go-to collaborative editor",
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <ClerkProvider
       appearance={{
         baseTheme: dark,
-        variables: { 
-          colorPrimary: "#3371FF" ,
-          fontSize: '16px'
+        variables: {
+          colorPrimary: "#3371FF",
+          fontSize: "16px",
+        },
+        layout: {
+          unsafe_disableDevelopmentModeWarnings: true,
         },
       }}
     >
@@ -35,11 +42,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             fontSans.variable
           )}
         >
-          <Provider>
-            {children}
-          </Provider>
+          <Provider>{children}</Provider>
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
